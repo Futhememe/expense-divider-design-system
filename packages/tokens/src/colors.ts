0ea01f0803be8ca2f5f4858941a3b183e82b21c6
Futhememe <kyoro_souza@hotmail.com>
2023-01-13 20:46:36 -0300

    🎉 init design system

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  white: '#FFF',
+  black: '#1B1F23',
+
+  green100: '#F7F8F2',
+  green200: '#EEF1E5',
+  green400: '#C8E386',
+  green500: '#8CAF30',
+
+  blue700: '#0C1E58',
+
+  purple400: '#E7E7FD',
+  purple600: '#9A80F8',
+  purple900: '#29273B',
+
+  red500: '#E54576',
+}
